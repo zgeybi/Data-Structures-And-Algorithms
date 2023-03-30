4c049c27fbb546431dbf1822efabc86f88432752
Jerry Treeman <35148370+TieBeTie@users.noreply.github.com>
2023-03-30 14:20:17 +0300

    Update reverseNum.cpp

--- reverseNum.cpp
+++ reverseNum.cpp
@@ -3,14 +3,8 @@
 int main()
 {
     unsigned long long num, reversed_num = 0;
-    std::cin >> num;
-    for(;num != 0;)
-    {
-        reversed_num = (reversed_num * 10) + (num%10);
-        num /= 10;
+    for (std::cin >> num; num != 0; num /= 10) {
+        reversed_num = reversed_num * 10 + num % 10;
     }
-
     std::cout << reversed_num << "\n";
-
-    return 0;
 }
