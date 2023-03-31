c89c1e78c55c8428b1ec9694ba8c7e788d219abb
zgeibi.tim.samer <zgeibi.t@phystech.edu>
2023-03-31 21:17:51 +0300

    Required Changes

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -1,102 +1,87 @@
 #include <iostream>
 #include <string>
 
-struct Node
+struct Node {
+  char val_;
+  Node *next;
+  Node() {
+    val_ = ' ';
+    next = nullptr;
+  }
+  ~Node() { delete next; }
+};
+class List {
+private:
+  Node *root_;
 
-{
-    char val;
-    Node* next;
-
-    // Constructor
-    Node(std::string a)
-    {
-        for(int i = 0; i < a.size(); i++)
-        {
-            if('a' <= a[i] <= 'z' || 'A' <= a[i] <= 'Z' || '0' <= a[i] <= '9')
-            {
-                this->addElements(a[i]);
-            }
-        }
+public:
+  List() { root_ = new Node(); }
+  ~List() { Clear(root_); }
+  void Clear(Node *head) {
+    if (head->next == nullptr) {
+      head->~Node();
+      return;
     }
-    Node()
-    {
-        val = '\0';
-        next = nullptr;
+    Node *next = head->next;
+    head->~Node();
+    Clear(next);
+  }
+  void AddElements(std::string val) {
+    for (int i = 0; i < val.size(); i++) {
+      Node *temp = new Node();
+      temp->val_ = val[i];
+      temp->next = nullptr;
+      if (root_->val_ == ' ') {
+        root_ = temp;
+        continue;
+      }
+      Node *new_node = FindLastNode(root_);
+      new_node->next = temp;
     }
-
-    //Methods
-
-    void addElements(char _val)
-    {
-        Node* temp = this;
-        
-        if(temp == nullptr)
-        {
-            temp = new Node();
-            temp->val = _val;
-            temp->next = nullptr;
-            return;
-        }
-        
-        if(temp->next == nullptr)
-        {
-            Node* newNode = new Node();
-            newNode->val = _val;
-            newNode->next = nullptr;
-            temp->next = newNode;
-            return;
-        }
-        temp = temp->next;
-        temp->addElements(_val);
-        temp->~Node();
+  }
+  Node *FindLastNode(Node *head) {
+    Node *last = head;
+    if (last->next == nullptr) {
+      return last;
     }
-
-    void printElements()
-    {
-        Node* temp = this;
-        if(temp == nullptr || temp->val == '\0')
-        {
-            return;
-        }
-        if(temp->next == nullptr || temp->next->val == '\0')
-        {
-            std::cout << temp->val;
-            return;
-        }
-
-        std::cout << temp->val;
-        temp->next->printElements();
-        temp->~Node();
+    last = FindLastNode(last->next);
+    return last;
+  }
+  void printElements(Node *next) {
+    Node *temp = new Node();
+    temp = next;
+    if (temp->next == nullptr) {
+      std::cout << temp->val_;
+      return;
     }
-
-    Node* reverseList()
-    {
-        if(this->next == nullptr)
-        {
-            return this;
-        }
-
-        Node* temp = this->next->reverseList();
-        this->next->next = this;
-        Node* previous = nullptr;
-        this->next = previous;
-        previous->~Node();
-        return temp;
+    std::cout << temp->val_;
+    printElements(temp->next);
+  }
+  Node *reverseList(Node *head) {
+    if (head == nullptr || head->next == nullptr) {
+      return head;
     }
-    
+    Node *previous = nullptr;
+    Node *temp = reverseList(head->next);
+    head->next->next = head;
+    head->next = previous;
+    return temp;
+  }
+  Node *GetRoot() { return root_; }
+  void SetRoot(Node *root) { root_ = root; }
 };
 
+int main() {
+  std::string a;
+  std::getline(std::cin, a);
+  List *linked_list = new List();
+  linked_list->AddElements(a);
+  List *reversed = new List();
+  Node *head = reversed->GetRoot();
+  head = linked_list->reverseList(linked_list->GetRoot());
+  reversed->SetRoot(head);
+  reversed->printElements(head);
 
-
-int main()
-{
-    std::string a;
-    std::getline(std::cin, a);
-
-    Node* head = new Node(a);
-    Node* reversed = head->reverseList();
-    head->~Node();
-    reversed->printElements();
-
-    return 0;
+  return 0;
 }
+
